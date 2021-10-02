fcc05fadc93112dadc232b8800e2a3aefd5ecebd
philippe MEYER <pmg.meyer@gmail.com>
2021-10-02 15:41:06 +0200

    first attempt

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-api
  node API for my chess studies
+
+ npm i cors express nodemon
---
+++ public/index.js
@@ -0,0 +1,48 @@
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+// json file with the data
+const data = fs.readFileSync('./data/data.json');
+
+const chessGames = JSON.parse(data);
+const app = express();
+
+// To solve the cors issue
+app.set('port', 12000);
+app.listen(process.env.PORT,
+	() => console.log("Server Starts"));
+	
+app.use(express.static('public'));
+app.use(cors());
+app.get('/test', () =>{
+	response.send("ok");
+
+});
+// when get request is made, alldata() is called
+app.get('/games', alldata);
+
+function alldata(request, response) {
+	
+	// Returns all information about the chessGames
+	response.send(JSON.stringfy(chessGames));
+}
+
+app.get('/games/:id/', searchGame);
+
+function searchGame(request, response) {
+	let id = Number(request.params.games);
+    let reply = chessGames.filter((x) =>{
+        return x.id === id;
+    });
+	
+	if(reply.length === 0) {
+		reply = "";		
+	}else{
+        reply=JSON.stringfy(reply);
+    }
+	response.send(reply);
+}
